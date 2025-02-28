d8346be47b30949e3db33f46c3eac5217781ab07
Felipe Cholo <felipechologonzalez000@gmail.com>
2025-02-27 22:35:24 -0500

    Cambios Codigo Rama Ranation

--- src/Main.kt
+++ src/Main.kt
@@ -3,5 +3,13 @@ fun main() {
 
     println("Hello, " + name + "!")
 
+    val name1 = "Kotlim"
+
+    println("Holaaa, " + name1 + "!")
+
+    val names = "Kotlin"
+
+    println("Wennnas, " + names + "!")
+
 
 }
