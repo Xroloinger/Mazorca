185365a7fbe9a9c5abe49bae8cf73678574b1693
Felipe Cholo <felipechologonzalez000@gmail.com>
2025-02-27 22:41:15 -0500

    Segundo Cambio

--- src/Main.kt
+++ src/Main.kt
@@ -1,15 +1,6 @@
 fun main() {
     val name = "Kotlin"
-
     println("Hello, " + name + "!")
-
-    val name1 = "Kotlim"
-
-    println("Holaaa, " + name1 + "!")
-
     val names = "Kotlin"
-
     println("Wennnas, " + names + "!")
-
-
 }
